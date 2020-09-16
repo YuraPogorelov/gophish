4fc00873211f4244e2704cf7a6b7f1d09ac89d2e
Yura Pogorelov <pyurik44@gmail.com>
2020-09-16 15:40:48 +0300

    Your awesome message

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
 
   const getCampaign = async () => {
     const response = await fetch(
-      `https://dash.kronshtatd.ru:323/api/campaigns/?api_key=${API_KEY}`
+      `http://dash.kronshtatd.ru:323/api/campaigns/?api_key=${API_KEY}`
     );
     const data = await response.json();
     setCampaigns(data);
